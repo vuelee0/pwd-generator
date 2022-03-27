8b56c079f1b8373c91c83567d085110369a265fc
Vue Lee <vuelee09@gmail.com>
2022-03-26 19:27:27 -0500

    add if statements and for loop

--- Develop/script.js
+++ Develop/script.js
@@ -16,65 +16,71 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var getPwdCharArray = {
-  getLowerCase: function() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-  },
 
-  getUpperCase: function () {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-  },
 
-  getNumericChar: function() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-  },
 
-  getSpecialChar: function() {
-    var specialCharacters = ("!#$%&'()*+,-./:;<=>?@[\]^_`{|}~");
-    return specialCharacters[Math.floor(Math.random() * specialCharacters.passwordLength)];
+
+
+function generatePassword() {
+
+  var passwordLength = prompt("Please type a number between 8 and 128. This is your new password length.");
+  var lowerCase = confirm("Do you want your new password to include lowercase letters?");
+  var upperCase = confirm("Do you want your new password to include uppercase letters?");
+  var numericChar = confirm("Do you want your new password to include numbers?");
+  var specialChar = confirm("Do you want your new password to include special characters?");
+  var minimumCharacter = 8;
+
+  // function code for randomizing character stored in an array
+  var getPwdCharArray = {
+    getLowerCase: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    },
+  
+    getUpperCase: function () {
+      return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    },
+  
+    getNumericChar: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    },
+  
+    getSpecialChar: function() {
+      var specialCharacters = ("!#$%&'()*+,-./:;<=>?@[\]^_`{|}~");
+      return specialCharacters[Math.floor(Math.random() * specialCharacters.passwordLength)];
+    }
+  };
+
+
+  let newRandomPassword = "";
+
+  if (parseInt(passwordLength) > 8 || parseInt(passwordLength) > 128) {
+    newRandomPassword += passwordLength;
   }
+
+  if (lowerCase === true) {
+    newRandomPassword = getPwdCharArray.getLowerCase();
+  }
+
+  if (upperCase === true) {
+    newRandomPassword = getPwdCharArray.getUpperCase();
+  }
+
+  if (numericChar === true) {
+    newRandomPassword = getPwdCharArray.getNumericChar();
+  }
+
+  if (specialChar === true) {
+    newRandomPassword = getPwdCharArray.getSpecialChar();
+  }
+  
+
+  
+  for (let i = 0; i < (parseInt(passwordLength) - minimumCharacter); i++) {
+    var newRandomNum = Math.floor(Math.random() * 4);
+
+    newRandomPassword += newRandomNum;
+  }
+
+  return newRandomPassword;
+
 };
-
-
-var passwordLength = prompt("Please type a number between 8 and 128. This is your new password length.");
-var lowerCase = confirm("Do you want your new password to include lowercase letters?");
-var upperCase = confirm("Do you want your new password to include uppercase letters?");
-var numericChar = confirm("Do you want your new password to include numbers?");
-var specialChar = confirm("Do you want your new password to include special characters?");
-
-
-// function generatePassword() {
-//   let generatePassword = "";
-  
-//   if (lowerCase === true) {
-//     inputLowerCase = getPwdCharArray.getLowerCase();
-//   }
-
-//   if (upperCase === true) {
-//     inputUpperCase = getPwdCharArray.getUpperCase();
-//   }
-
-//   if (numericChar === true) {
-//     inputNumericChar = getPwdCharArray.getNumericChar();
-//   }
-
-//   if (specialChar === true) {
-//     inputSpecialChar = getPwdCharArray.getSpecialChar();
-//   }
-  
-
-//   let newRandomPassword = "";
-//   for (let i = 0; i < getPasswordLength; i++) {
-//     var newRandomNum = Math.floor(Math.random() * 4);
-
-//     newRandomPassword += newRandomNum;
-//   }
-
-//   newRandomPassword += inputLowerCase;
-//   newRandomPassword += inputUpperCase
-//   newRandomPassword += inputNumericChar
-//   newRandomPassword += inputSpecialChar
-
-//   return newRandomPassword;
-
-// };
