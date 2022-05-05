35e6a190f4594a2ca5f26c087206d46862f53d4f
Vue Lee <vuelee09@gmail.com>
2022-05-05 15:05:15 -0500

    fix script.js file to function correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,24 +8,36 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-
+// function to generate the password base on user's criteria selection
 function generatePassword() {
 
   var passwordLength = prompt("Please type a number between 8 and 128. This is your new password length.");
-  var pwdLengthInt = Math.ceil(parseInt(passwordLength) / 5);
+  var pwdLengthInt = parseInt(passwordLength);
+
+    if (pwdLengthInt < 8 || pwdLengthInt > 128 || isNaN(pwdLengthInt)) {
+      alert("Password must be at least 8 charaters, less than 128 characters and must be a number.")
+      generatePassword()
+    }
+
   var lowerCase = confirm("Do you want your new password to include lowercase letters?");
   var upperCase = confirm("Do you want your new password to include uppercase letters?");
   var numericChar = confirm("Do you want your new password to include numbers?");
   var specialChar = confirm("Do you want your new password to include special characters?");
+
+    if (!lowerCase && !upperCase && !numericChar && !specialChar) {
+      alert("You must choose at least one option.")
+      generatePassword()
+    }
+
+  console.log("After if")
 
   // function code for randomizing character stored in an array
   var getPwdCharArray = {
@@ -47,28 +59,30 @@ function generatePassword() {
     }
   };
 
-  let newRandomPassword = "";
-  for (let i = 0; i < pwdLengthInt; i++) {
-  
-    if (pwdLengthInt >= 8 || pwdLengthInt <= 128) {
-      newRandomPassword += pwdLengthInt;
-    }
-
+  // push selected characters to for loop
+  var userChoices = []
     if (lowerCase === true) {
-      newRandomPassword += getPwdCharArray.getLowerCase();
+      userChoices.push(getPwdCharArray.getLowerCase);
     }
 
     if (upperCase === true) {
-      newRandomPassword += getPwdCharArray.getUpperCase();
+      userChoices.push(getPwdCharArray.getUpperCase);
     }
 
     if (numericChar === true) {
-      newRandomPassword += getPwdCharArray.getNumericChar();
+      userChoices.push(getPwdCharArray.getNumericChar);
     }
 
     if (specialChar === true) {
-      newRandomPassword += getPwdCharArray.getSpecialChar()
+      userChoices.push(getPwdCharArray.getSpecialChar);
     }
+    console.log(userChoices)
+
+  // for loop to iterate through the array
+  var newRandomPassword = "";
+    for (var i = 0; i < pwdLengthInt; i++) {
+      console.log(i)
+      newRandomPassword = newRandomPassword + userChoices[Math.floor(Math.random()*userChoices.length)]()
   }
 
   return newRandomPassword;
